cb55562780316c53027168a34395fb708e2b3006
Debbie Michel and Elrey Belmonti <elrey@>
2017-09-05 16:40:39 -0700

    It will have 3 more specs to follow

--- spec/leetspeak_spec.rb
+++ spec/leetspeak_spec.rb
@@ -18,4 +18,18 @@ describe('String#leetspeak') do
   it('replaces every "I" in a string with a "1"') do
     expect("I like Ike".leetspeak).to eq("1 lik3 1k3")
   end
+
+
+
+  # it('replaces every "s" in a string with a "z"') do
+  #   expect(leetspeak("roses")).to(eq("r0z3z"))
+  # end
+  #
+  # it('does NOT replace the first letter when it is an s') do
+  #   expect(leetspeak("sassafrass")).to(eq("sazzafrazz"))
+  # end
+  #
+  # it('replaces letters correctly in a string of words') do
+  #   expect(leetspeak("I scream you scream we all scream for raspberry ice cream.")).to(eq("1 scr3am y0u scr3am w3 all scr3am f0r razpb3rry ic3 cr3am"))
+  # end
 end
