4401e18eab11d3d4cc2fa8d578a24f88abc405b6
Debbie Michel and Elrey Belmonti <debbie@>
2017-09-05 15:53:49 -0700

    Turn the letter e into a 3

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -3,18 +3,19 @@ class String
 
 def leetspeak
   # The code below will break out the word happy that comes from the spec file into an array ["h", "a", "p", "p", "y"]
-      # word = self.chomp
-      # word = word.chars
-      # word
-      binding.pry
+  word = self.chomp
+  word = word.chars
+  new_word = []
+  #When using only the self word it will return the word "happy" from the spec file
+  # self
 
-  self
-end
-  # word = self
-  #  word.each() do |i|
-  #   if i == "e"
-  #     self.push(3)
-  #   else
-  #     self.push(i)
-  #   end
+  word.each() do |i|
+    if i == "e"
+      new_word.push(3)
+    else
+      new_word.push(i)
+    end
+  end
+  return new_word.join
+    end
   end
--- spec/leetspeak_spec.rb
+++ spec/leetspeak_spec.rb
@@ -5,7 +5,8 @@ describe('String#leetspeak') do
   it('returns a string as is when no leet_speak rules apply') do
     expect("happy".leetspeak).to eq("happy")
   end
-  # it('replaces every "e" in a string with a "3"') do
-  #   expect("elephant".leetspeak).to eq("3l3phant")
-  # end
+
+  it('replaces every "e" in a string with a "3"') do
+    expect("elephant".leetspeak).to eq("3l3phant")
+  end
 end
